b54372c3e1f073fa12a739151c1b5e14bdab75ff
ANUSRINI-123 <65034830+ANUSRINI-123@users.noreply.github.com>
2020-06-05 19:03:19 +0530

    Create Program 1.java

---
+++ Day 7 Lab Programs/Logical Building 1-Day 6/Program 1.java
@@ -0,0 +1,27 @@
+import java.io.*;
+import  java.util.*;
+class UserMainCode
+{
+  public int isPrime(int input1)
+  {
+      boolean flag=true;
+		for(int i=2;i<=input1/2;i++)
+		{
+			if(input1%i==0)
+			{
+				flag=false;
+				break;
+			}
+
+		}
+		if(flag)
+		{
+			return 2;
+
+		}
+		else
+		{
+			return 1;
+		}
+	}
+}
