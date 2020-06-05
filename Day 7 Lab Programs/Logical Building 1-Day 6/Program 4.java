4ec23bc908af7c335f450a86055525876819646f
ANUSRINI-123 <65034830+ANUSRINI-123@users.noreply.github.com>
2020-06-05 21:02:08 +0530

    Update Program 4.java

--- Day 7 Lab Programs/Logical Building 1-Day 6/Program 4.java
+++ Day 7 Lab Programs/Logical Building 1-Day 6/Program 4.java
@@ -1,25 +1,28 @@
 import java.io.*;
 import  java.util.*;
-lass UserMainCode
+class UserMainCode
 {
 	public int isPrime(int input1)
 	{
 		int num, count, i;
-    num=1;
-    count=0;
- 
-    while (count < input1){
-      num=num+1;
-      for (i = 2; i <= num; i++){ //Here we will loop from 2 to num
-        if (num % i == 0) {
-          break;
-        }
-      }
-      if ( i == num){//if it is a prime number
-        count = count+1;
-      }
-    }
-    return  num;
-    }
-    }
+    		n=1;
+    		count=0;
+		while (count < input1)
+		{
+      			n=n+1;
+      			for (i = 2; i <= n; i++)
+			{ 
+        			if (n % i == 0)
+				{
+         				break;
+     				}
+    			}
+      			if ( i == n)
+			{
+        			count = count+1;
+      			}
+    		}
+    		return  n;
+    	}
+}
 		
