5a27585b5e6f0f6b5cfd26aa2a908d854ad0f76d
ANUSRINI-123 <65034830+ANUSRINI-123@users.noreply.github.com>
2020-06-05 19:19:46 +0530

    Update Program 2.java

--- Day 7 Lab Programs/Logical Building 1-Day 6/Program 2.java
+++ Day 7 Lab Programs/Logical Building 1-Day 6/Program 2.java
@@ -2,9 +2,9 @@ import java.io.*;
 import  java.util.*;
 class UserMainCode
 {
-  public int nFactorial(int input1)
-  {
-    int fact=1;
+	public int nFactorial(int input1)
+  	{
+    		int fact=1;
 		for(int i=1;i<=input1;i++)
 		{
 			fact=fact*i;
