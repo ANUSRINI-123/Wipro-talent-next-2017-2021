36d63834d5aea9f95a5d80eca698bd9be209039d
ANUSRINI-123 <65034830+ANUSRINI-123@users.noreply.github.com>
2020-06-05 18:21:28 +0530

    Update Program 4.java

--- Day 7 Lab Programs/Logical Building 1-Day 5/Program 4.java
+++ Day 7 Lab Programs/Logical Building 1-Day 5/Program 4.java
@@ -2,34 +2,34 @@ import java.io.*;
 import  java.util.*;
 class UserMainCode
 {
-public int countEvensOdds(int input1,int input2,int input3,int input4,int input5,String input6)
-{
-int arr[]={input1,input2,input3,input4,input5};
+	public int countEvensOdds(int input1,int input2,int input3,int input4,int input5,String input6)
+	{
+		int arr[]={input1,input2,input3,input4,input5};
 		int even=0,odd=0;
 		String str=new String("even");
 		if(str==input6)
 		{
-		for(int i=0;i<5;i++)
-		{
-		if(arr[i]%2==0)
-		{
-			even=even+1;
-		}
-		}
-		return even;
+			for(int i=0;i<5;i++)
+			{
+				if(arr[i]%2==0)
+				{
+					even=even+1;
+				}
+			}
+			return even;
 		}
 	
 		else
 		{
-		for(int i=0;i<5;i++)
-		{
-		if(arr[i]%2!=0)
-		{
-			odd=odd+1;
-		}
+			for(int i=0;i<5;i++)
+			{
+				if(arr[i]%2!=0)
+				{
+					odd=odd+1;
+				}
 		
-		}
-		return odd;
+			}
+			return odd;
 		}
 	}
 }
