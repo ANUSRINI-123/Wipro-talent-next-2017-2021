39da327d9339a7bde27bbe6a8eb334a5eb570440
ANUSRINI-123 <65034830+ANUSRINI-123@users.noreply.github.com>
2020-06-05 17:30:37 +0530

    Update Program 2.java

--- Day 7 Lab Programs/Logical Building 1-Day 5/Program 2.java
+++ Day 7 Lab Programs/Logical Building 1-Day 5/Program 2.java
@@ -4,15 +4,15 @@ class UserMainCpde
 {
   public int countEvens(int input1,int input2,int input3,int input4,int input5)
   {
-    int arr[]={input1,input2,input3,input4,input5};
-		int sum=0;
-		for(int i=0;i<5;i++)
+  	int arr[]={input1,input2,input3,input4,input5};
+	int sum=0;
+	for(int i=0;i<5;i++)
+	{
+		if(arr[i]%2==0)
 		{
-			if(arr[i]%2==0)
-			{
-				sum=sum+1;
-			}
+			sum=sum+1;
 		}
-		return sum;
 	}
+	return sum;
+  }
 }
