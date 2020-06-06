1630e068f19a83e4b89660fa531c99bfe51d13f6
ANUSRINI-123 <65034830+ANUSRINI-123@users.noreply.github.com>
2020-06-06 18:35:37 +0530

    Update Program 3.java

--- Day 8 Lab Programs/Logical Building 1-Day 9/Program 3.java
+++ Day 8 Lab Programs/Logical Building 1-Day 9/Program 3.java
@@ -1,72 +1,74 @@
 import java.io.*;
 import  java.util.*;
-class UsermainCode{
-public int getPin(int input1, int input2, int input3) {
-		
-		int pin = 0;
-		
-		
-		int ip1 = Integer.parseInt(String.valueOf(String.valueOf(input1).charAt(2)));
-		int ip2 = Integer.parseInt(String.valueOf(String.valueOf(input2).charAt(2)));
-		int ip3 = Integer.parseInt(String.valueOf(String.valueOf(input3).charAt(2)));
-		
-		if (ip1 < ip2 && ip1 < ip3) {
-			pin = ip1;
-		} else if (ip2 < ip3) {
-			pin = ip2;
-		} else {
-			pin = ip3;
-		}
-		
-		
-		ip1 = Integer.parseInt(String.valueOf(String.valueOf(input1).charAt(1)));
-		ip2 = Integer.parseInt(String.valueOf(String.valueOf(input2).charAt(1)));
-		ip3 = Integer.parseInt(String.valueOf(String.valueOf(input3).charAt(1)));
-		
-		if (ip1 < ip2 && ip1 < ip3) {
-			pin = ip1 * 10 + pin;
-		} else if (ip2 < ip3) {
-			pin = ip2 * 10 + pin;
-		} else {
-			pin = ip3 * 10 + pin;
-		}
-		
-	
-		ip1 = Integer.parseInt(String.valueOf(String.valueOf(input1).charAt(0)));
-		ip2 = Integer.parseInt(String.valueOf(String.valueOf(input2).charAt(0)));
-		ip3 = Integer.parseInt(String.valueOf(String.valueOf(input3).charAt(0)));
-		
-		if (ip1 < ip2 && ip1 < ip3) {
-			pin = ip1 * 100 + pin;
-		} else if (ip2 < ip3) {
-			pin = ip2 * 100 + pin;
-		} else {
-			pin = ip3 * 100 + pin;
-		}
-		
-		
-		int maxOfAll = 0;
-		for (int i = 0; i < 3; i++) {
-			ip1 = Integer.parseInt(String.valueOf(String.valueOf(input1).charAt(i)));
-			ip2 = Integer.parseInt(String.valueOf(String.valueOf(input2).charAt(i)));
-			ip3 = Integer.parseInt(String.valueOf(String.valueOf(input3).charAt(i)));
-			
-
-			if (ip1 > ip2 && ip1 > ip3) {
-				maxOfAll = ip1 > maxOfAll ? ip1 : maxOfAll;
-			} else if (ip2 > ip3) {
-				maxOfAll = ip2 > maxOfAll ? ip2 : maxOfAll;
-			} else {
-				maxOfAll = ip3 > maxOfAll ? ip3 : maxOfAll;
-			}
-			
-		}
-        
-		pin = maxOfAll * 1000 + pin;
-		
-		
-		return pin;
+class UsermainCode
+{
+public int getPin(int input1, int input2, int input3)
+{
+	int pin = 0;
+	int ipt1 = Integer.parseInt(String.valueOf(String.valueOf(input1).charAt(2)));
+	int ipt2 = Integer.parseInt(String.valueOf(String.valueOf(input2).charAt(2)));
+	int ipt3 = Integer.parseInt(String.valueOf(String.valueOf(input3).charAt(2)));
+	if (ipt1 < ipt2 && ipt1 < ipt3)
+	{
+		pin = ipt1;
+	} 
+	else if (ipt2 < ipt3)
+	{
+		pin = ipt2;
+	} else 
+	{
+		pin = ipt3;
 	}
-	
-	
+	ipt1 = Integer.parseInt(String.valueOf(String.valueOf(input1).charAt(1)));
+	ipt2 = Integer.parseInt(String.valueOf(String.valueOf(input2).charAt(1)));
+	ipt3 = Integer.parseInt(String.valueOf(String.valueOf(input3).charAt(1)));
+	if (ipt1 < ipt2 && ipt1 < ipt3)
+	{
+		pin = ipt1 * 10 + pin;
+	} 
+	else if (ipt2 < ipt3) 
+	{
+		pin = ipt2 * 10 + pin;
 	}
+	else 
+	{
+		pin = ipt3 * 10 + pin;
+	}
+	ipt1 = Integer.parseInt(String.valueOf(String.valueOf(input1).charAt(0)));
+	ipt2 = Integer.parseInt(String.valueOf(String.valueOf(input2).charAt(0)));
+	ipt3 = Integer.parseInt(String.valueOf(String.valueOf(input3).charAt(0)));
+	if (ipt1 < ipt2 && ipt1 < ipt3)
+	{
+		pin = ipt1 * 100 + pin;
+	}
+	else if (ipt2 < ipt3)
+	{
+		pin = ipt2 * 100 + pin;
+	}
+	else 
+	{
+		pin = ipt3 * 100 + pin;
+	}
+	int maxOfAll = 0;
+	for (int i = 0; i < 3; i++)
+	{
+		ipt1 = Integer.parseInt(String.valueOf(String.valueOf(input1).charAt(i)));
+		ipt2 = Integer.parseInt(String.valueOf(String.valueOf(input2).charAt(i)));
+		ipt3 = Integer.parseInt(String.valueOf(String.valueOf(input3).charAt(i)));
+		if (ipt1 > ipt2 && ipt1 > ipt3)
+		{
+			maxOfAll = ipt1 > maxOfAll ? ipt1 : maxOfAll;
+		} 
+		else if (ipt2 > ipt3) 
+		{
+			maxOfAll = ipt2 > maxOfAll ? ipt2 : maxOfAll;
+		} 
+		else 
+		{
+			maxOfAll = ipt3 > maxOfAll ? ipt3 : maxOfAll;
+		}
+	}
+        pin = maxOfAll * 1000 + pin;
+	return pin;
+}
+}
