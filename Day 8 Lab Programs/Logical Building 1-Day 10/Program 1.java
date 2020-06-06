a438ff84a1cd19692e3c2f26d7133ad9eb88da1f
ANUSRINI-123 <65034830+ANUSRINI-123@users.noreply.github.com>
2020-06-06 18:55:47 +0530

    Update Program 1.java

--- Day 8 Lab Programs/Logical Building 1-Day 10/Program 1.java
+++ Day 8 Lab Programs/Logical Building 1-Day 10/Program 1.java
@@ -2,16 +2,14 @@ import java.io.*;
 import  java.util.*;
 class UserMainCode
 {
-public String secondWordUpperCase
-{
-String[] a=input1.split(" ");
-        if(a.length<=1)
-        {
-            return "LESS";
-        }
-        String word=a[1].toUpperCase();
-        return word;
-    }
-
-	
-	}
+	public String secondWordUpperCase
+	{
+		String[] a=input1.split(" ");
+        	if(a.length<=1)
+        	{
+           		return "LESS";
+        	}
+        	String word=a[1].toUpperCase();
+        	return word;
+    	}
+}
