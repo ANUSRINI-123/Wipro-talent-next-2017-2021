15c0bcc669edef0d9bf98e417bf22062ffaec33e
ANUSRINI-123 <65034830+ANUSRINI-123@users.noreply.github.com>
2020-06-06 16:57:37 +0530

    Update Program 1.java

--- Day 8 Lab Programs/Logical Building 1-Day 9/Program 1.java
+++ Day 8 Lab Programs/Logical Building 1-Day 9/Program 1.java
@@ -2,9 +2,9 @@ import java.io.*;
 import  java.util.*;
 class UserMainCode
 {
-public int palindromeOrNot(int input1)
-{
-int s=0,t,rem;
+	public int palindromeOrNot(int input1)
+	{
+		int s=0,t,rem;
 		t=input1;
 		while(input1>0)
 		{
