b09578462d3739c7b91631e89d4cf2aa3f2d6fad
ANUSRINI-123 <65034830+ANUSRINI-123@users.noreply.github.com>
2020-06-06 13:24:38 +0530

    Update Program 2.java

--- Day 8 Lab Programs/Logical Building 1-Day 7/Program 2.java
+++ Day 8 Lab Programs/Logical Building 1-Day 7/Program 2.java
@@ -2,13 +2,11 @@ import java.io.*;
 import  java.util.*;
 class UserMainCode
 {
-public int allDigitsCount(int input1)
-{
-int count=0;
+	public int allDigitsCount(int input1)
+	{
+		int count=0;
 		while(input1!=0)
 		{
-			
-			
 			input1=input1/10;
 			count++;
 		}
