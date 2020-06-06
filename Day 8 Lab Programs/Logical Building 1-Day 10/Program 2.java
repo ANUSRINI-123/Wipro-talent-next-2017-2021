f2c3e3cda8402f92d63311fab4e17e346a45b4c5
ANUSRINI-123 <65034830+ANUSRINI-123@users.noreply.github.com>
2020-06-06 19:05:16 +0530

    Update Program 2.java

--- Day 8 Lab Programs/Logical Building 1-Day 10/Program 2.java
+++ Day 8 Lab Programs/Logical Building 1-Day 10/Program 2.java
@@ -4,20 +4,18 @@ class UserMainCode
 {
 public int isPalindrome(String input1)
 {
-String r="";
-		for(int i=input1.length()-1;i>=0;i--)
-		{
-			r+=input1.charAt(i);
-		}
-		if(r.equalsIgnoreCase(input1))
-		{
-			return 2;
-		}
-		else
-		{
-			return 1;
-		}
-	
-	
+	String r="";
+	for(int i=input1.length()-1;i>=0;i--)
+	{
+		r+=input1.charAt(i);
 	}
+	if(r.equalsIgnoreCase(input1))
+	{
+		return 2;
+	}
+	else
+	{
+		return 1;
+	}
+}
 }
