acf02db60229879123bfa038170a814d0643ded0
ANUSRINI-123 <65034830+ANUSRINI-123@users.noreply.github.com>
2020-06-06 18:45:07 +0530

    Update Program 4.java

--- Day 8 Lab Programs/Logical Building 1-Day 9/Program 4.java
+++ Day 8 Lab Programs/Logical Building 1-Day 9/Program 4.java
@@ -2,9 +2,9 @@ import java.io.*;
 import  java.util.*;
 class UserMainCode
 {
-public int totalHillWeight
-{
-int r=input1;
+	public int totalHillWeight
+	{
+		int r=input1;
 		int f=input2;
 		int w,totalweight=0;
 		for(int i=2;i<=r;i++)
@@ -15,5 +15,4 @@ int r=input1;
 		}
 		return totalweight+input2;
 	}
-	
-	}
+}
