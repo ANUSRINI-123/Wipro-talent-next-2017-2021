03065ae3743ec559b1533718555523dbb7fa133a
ANUSRINI-123 <65034830+ANUSRINI-123@users.noreply.github.com>
2020-06-09 21:22:30 +0530

    Update Program 3.java

--- Day 11 Lab Programs/Program 3.java
+++ Day 11 Lab Programs/Program 3.java
@@ -1,29 +1,33 @@
 import java.io.*;
 import  java.util.*;
-class UserMainCode{
-public int findPassword(int input1, int input2,int input3, int input4,int input5){
-    int n1 = input1 +input2 +input3 +input4 +input5;
-		int n2 =sum(input1)+sum(input2)+sum(input3)+sum(input4)+sum(input5);
-		int n3 = n1-n2;
-		return n2-n3;
+class UserMainCode
+{
+	public int findPassword(int input1, int input2,int input3, int input4,int input5)
+	{
+    		int x1 = input1 +input2 +input3 +input4 +input5;
+		int x2 =sum(input1)+sum(input2)+sum(input3)+sum(input4)+sum(input5);
+		int x3 = x1-x2;
+		return x2-x3;
 	}
-	public static int sum(int n){
-		int m=n;
-		int[] a = new int[10];
+	public static int sum(int x)
+	{
+		int m=x;
+		int[] a1 = new int[10];
 		int k=0;
-		while(n!=0){
-			int r = n%10;
-			a[r]++;
-			if(a[r]>0)
-				k=a[r];
-			n /= 10;
+		while(n!=0)
+		{
+			int r = x%10;
+			a1[r]++;
+			if(a1[r]>0)
+				k=a1[r];
+			x /= 10;
 		}
-		for(int j=0;j<10;j++){
-			if(a[j]==0 ||a[j]==k)
+		for(int j=0;j<10;j++)
+		{
+			if(a1[j]==0 ||a1[j]==k)
 				continue;
-			else{
+			else
 				return 0;
-			}
 		}
 		return m;
 	}
