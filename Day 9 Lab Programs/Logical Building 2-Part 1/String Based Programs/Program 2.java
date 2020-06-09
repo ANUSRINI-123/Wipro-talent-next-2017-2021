514763e6a61af75223b266177172c7c8bd60f550
ANUSRINI-123 <65034830+ANUSRINI-123@users.noreply.github.com>
2020-06-09 07:49:40 +0530

    Update Program 2.java

--- Day 9 Lab Programs/Logical Building 2-Part 1/String Based Programs/Program 2.java
+++ Day 9 Lab Programs/Logical Building 2-Part 1/String Based Programs/Program 2.java
@@ -4,22 +4,22 @@ class UserMainCode
 {
   public int codeThroughStrings(String input1)
   {
-		int count=0,val=0;;
-		input1=input1.toUpperCase();
-		String[] s=input1.split(" ");
-		for(int i=0;i<s.length;i++)
-		{
-			count+=s[i].length();
-		}
-		while(count/10!=0)
-		{
-			while(count!=0)
-			{
-				val+=count%10;
-				count/=10;
-			}	
-			count=val;
-		}
-		return count;
+	int c=0,value=0;;
+	input1=input1.toUpperCase();
+	String[] a=input1.split(" ");
+	for(int i=0;i<a.length;i++)
+	{
+		c+=a[i].length();
 	}
+	while(c/10!=0)
+	{
+		while(c!=0)
+		{
+			value+=c%10;
+			c/=10;
+		}	
+		c=value;
+	}
+	return c;
+}
 }
