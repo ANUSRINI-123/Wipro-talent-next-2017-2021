2da3f01022f9bfd4488727f8cf57600d61629abf
ANUSRINI-123 <65034830+ANUSRINI-123@users.noreply.github.com>
2020-06-09 07:45:43 +0530

    Update Program 1.java

--- Day 9 Lab Programs/Logical Building 2-Part 1/String Based Programs/Program 1.java
+++ Day 9 Lab Programs/Logical Building 2-Part 1/String Based Programs/Program 1.java
@@ -4,42 +4,42 @@ class UserMainCode
 {
   public int findStringCode(String input1)
   {
-		String[] s = input1.split(" ");
-		StringBuffer output = new StringBuffer();
-		for (String word : s)
-    {
-			int sum = 0;
-			for (int i = 0; i < (word.length() / 2); i++)
-      {
-				int j = word.length() - i - 1; 
-				int lar;
-				int small;
-				if (letterToNo(word.charAt(i)) > letterToNo(word.charAt(j))) 
-        {
-					lar = letterToNo(word.charAt(i));
-					small = letterToNo(word.charAt(j));
-				}
-        else
-        {
-					lar = letterToNo(word.charAt(j));
-					small = letterToNo(word.charAt(i));
-				}
-				sum += lar - small;
+	String[] s = input1.split(" ");
+	StringBuffer output = new StringBuffer();
+	for (String word : s)
+   	{
+		int sum = 0;
+		for (int i = 0; i < (word.length() / 2); i++)
+     		{
+			int j = word.length() - i - 1; 
+			int lar;
+			int small;
+			if (letterToNo(word.charAt(i)) > letterToNo(word.charAt(j))) 
+        		{
+				lar = letterToNo(word.charAt(i));
+				small = letterToNo(word.charAt(j));
 			}
-			if (word.length() % 2 == 1)
-      {
-				sum += letterToNo(word.charAt(word.length() / 2));
+        		else
+        		{
+				lar = letterToNo(word.charAt(j));
+				small = letterToNo(word.charAt(i));
 			}
-			output.append(sum);
+			sum += lar - small;
 		}
-		return Integer.parseInt(output.toString());
+		if (word.length() % 2 == 1)
+     		{
+			sum += letterToNo(word.charAt(word.length() / 2));
+		}
+		output.append(sum);
 	}
-	public static int letterToNo(char ch)
-  {
-		if (ch >= 65 && ch <= 90)
-			return ch - 64;
-		if (ch >= 97 && ch <= 122)
-			return ch - 96;
-		return 0;
-	}
+	return Integer.parseInt(output.toString());
+}
+public static int letterToNo(char ch)
+{
+	if (ch >= 65 && ch <= 90)
+		return ch - 64;
+	if (ch >= 97 && ch <= 122)
+		return ch - 96;
+	return 0;
+}
 }
