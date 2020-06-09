11f58582bbb9fae80fc338a9635ffe9a066068e3
ANUSRINI-123 <65034830+ANUSRINI-123@users.noreply.github.com>
2020-06-09 12:28:54 +0530

    Update Program 1.java

--- Day 10 Lab Programs/Logical Building 2-Part 2/String Based Programs/Program 1.java
+++ Day 10 Lab Programs/Logical Building 2-Part 2/String Based Programs/Program 1.java
@@ -1,27 +1,25 @@
 import java.io.*;
 import  java.util.*;
-class UserMainCode{
-public int identifyPossibleString(String input1, String input2){
-    input1 = input1.toUpperCase();
-		input2 = input2.toUpperCase();
-		String[] a = input2.split(":");
-		int index = input1.indexOf("_");
-		String s="";
-		for(int i = 0;i<a.length;i++){
-			if(a[i].length()!=input1.length())
-				continue;
-			String s1=input1;
-			char ch = a[i].charAt(index);
-			s1 = s1.replace('_',ch);
-			if(s1.equals(a[i])){
-				if(s.isEmpty())
-					s +=s1;
-				else
-					s += ":"+s1;
+class UserMainCode
+{
+	public String identifyPossibleWords(String input1,String input2)
+	{
+		input1 = input1.toUpperCase();
+		StringBuffer op = new StringBuffer();
+		String[] w = input2.split(":");
+		int underscoreIndex = input1.indexOf('_');
+		for (int i = 0; i < w.length; i++) 
+		{
+			w[i] = w[i].toUpperCase();
+			if (w[i].length() >= input1.length() &&
+			input1.replace('_', w[i].charAt(underscoreIndex)).equals(w[i]))
+			{
+				op.append(w[i]).append(":");
 			}
 		}
-		if(s.isEmpty())
-			s="ERROR-009";
-		return s;
+		if (op.length() == 0) 
+			return "ERROR-009";
+        	else 
+			return op.toString().substring(0, op.length() - 1);
 	}
 }
