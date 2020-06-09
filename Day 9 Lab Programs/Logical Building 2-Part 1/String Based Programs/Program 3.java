de7c4fe9ec3bc281f6f8c35f24cd28c5d4262e29
ANUSRINI-123 <65034830+ANUSRINI-123@users.noreply.github.com>
2020-06-09 07:53:44 +0530

    Update Program 3.java

--- Day 9 Lab Programs/Logical Building 2-Part 1/String Based Programs/Program 3.java
+++ Day 9 Lab Programs/Logical Building 2-Part 1/String Based Programs/Program 3.java
@@ -1,16 +1,11 @@
 import.java.io.*;
 import java.io.util;
-class UserMainCode{
-public String addNumberstrings(String input1,String input2){
-	
-		BigDecimal a = new BigDecimal(input1);
-		BigDecimal b = new BigDecimal(input2);
-		
-		return String.valueOf(a.add(b));
-		
-		
-		
+class UserMainCode
+{
+	public String addNumberstrings(String input1,String input2)
+	{
+		BigDecimal x = new BigDecimal(input1);
+		BigDecimal y = new BigDecimal(input2);
+		return String.valueOf(x.add(y));
 	}
-	
-	
 }
