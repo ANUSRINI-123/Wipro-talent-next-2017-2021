32d9410a445f0c280e093d55e7e5212a8299a4d4
ANUSRINI-123 <65034830+ANUSRINI-123@users.noreply.github.com>
2020-06-09 08:16:48 +0530

    Update Program 3.java

--- Day 9 Lab Programs/Logical Building 2-Part 1/Array Based Programs/Program 3.java
+++ Day 9 Lab Programs/Logical Building 2-Part 1/Array Based Programs/Program 3.java
@@ -1,26 +1,32 @@
 import java.io.*;
 import  java.util.*;
-class UserMainCode{
-public int freqOccured(int[] input1, int input2){
-int[] a = new int[10];
-		for(int i=0;i<input2;i++){
-			count(input1[i],a);
-		}
-		int max=0,c=0;
-		for(int i=0;i<10;i++){
-			if(a[i]>=max){
-				max=a[i];
-				c=i;
-			}
-		}
-		return c;
+class UserMainCode
+{
+	public int freqOccured(int[] input1, int input2){
+	int[] x = new int[10];
+	for(int i=0;i<input2;i++)
+	{
+		count(input1[i],x);
 	}
-	public static int[] count(int n,int ar[]){
-		while(n!=0){
-			int r = n % 10;
-			ar[r]++;
-			n /= 10;
+	int max=0,c=0;
+	for(int i=0;i<10;i++)
+	{
+		if(x[i]>=max)
+		{
+			max=x[i];
+			c=i;
 		}
-		return ar;
 	}
+	return c;
+}
+public static int[] count(int n,int ar[])
+{
+	while(n!=0)
+	{
+		int r = n % 10;
+		ar[r]++;
+		n /= 10;
+	}
+	return ar;
+}
 }
