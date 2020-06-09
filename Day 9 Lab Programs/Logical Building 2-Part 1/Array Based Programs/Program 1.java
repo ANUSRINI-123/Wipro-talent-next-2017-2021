0ee428be589c89a264a199028e908438e3010254
ANUSRINI-123 <65034830+ANUSRINI-123@users.noreply.github.com>
2020-06-09 08:00:16 +0530

    Update Program 1.java

--- Day 9 Lab Programs/Logical Building 2-Part 1/Array Based Programs/Program 1.java
+++ Day 9 Lab Programs/Logical Building 2-Part 1/Array Based Programs/Program 1.java
@@ -1,26 +1,30 @@
 import java.io.*;
 import  java.util.*;
-class UserMainCode{
-public class Resul{
-  public final int output1;
-  public final int output2;
-  public Result(int out1,int out2){
-    output1=out1;
-    output2=out2;
-  }
+class UserMainCode
+{
+	public class encodedArrays
+	{
+		public final int op1;
+  		public final int op2;
+  		public Result(int ot1,int ot2){
+    		op1=ot1;
+    		op2=ot2;
+  	}
 }
-public Result findOriginalFirstandSum(int[] input1, int input2){
-  int sum=0;
-		int[] a = new int[input2];
-		int j = input2-1;
-		a[input2-1] = input1[input2-1];
-		for(int i=input2-2;i>=0;i--){
-			a[i] = input1[j-1]-a[i+1];
-			j--;
-		}
-		for(int i=0;i<input2;i++)
-			sum += a[i];
-		Result r = new Result(a[0],sum);
+public Result simpleEncodedArray(int[] input1, int input2)
+{
+	int s=0;
+	int[] a = new int[input2];
+	int j = input2-1;
+	a[input2-1] = input1[input2-1];
+	for(int i=input2-2;i>=0;i--)
+	{
+		a[i] = input1[j-1]-a[i+1];
+		j--;
+	}
+	for(int i=0;i<input2;i++)
+		s += a[i];
+	Result r = new Result(a[0],s);
 		return r;
     }
 }
